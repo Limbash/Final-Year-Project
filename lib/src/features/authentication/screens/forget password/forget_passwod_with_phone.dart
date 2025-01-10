35cd859bf1218884b0b893ffc539638cec35f696
Limbash <limbashcomty@gmail.com>
2025-01-10 23:34:14 +0100

    Handled some typos and some few things

--- lib/src/features/authentication/screens/forget password/forget_passwod_with_phone.dart
+++ lib/src/features/authentication/screens/forget password/forget_passwod_with_phone.dart
@@ -10,42 +10,40 @@ class ForgetPasswordWithPhone extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(tDefaultSize),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Icon(Icons.phone, size: 120),
-                Text(tForgetWithPhoneTitle,
-                    style: Theme.of(context).textTheme.titleLarge),
-                Text(tForgetWithPhoneNumberSubTitle,
-                    style: Theme.of(context).textTheme.titleSmall),
-                const SizedBox(height: tDefaultSize - 10),
-                TextFormField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.phone),
-                    hintText: "Phone Number",
-                    border: OutlineInputBorder(),
-                  ),
+        body: Padding(
+          padding: const EdgeInsets.all(tDefaultSize),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(Icons.phone, size: 120),
+              Text(tForgetWithPhoneTitle,
+                  style: Theme.of(context).textTheme.titleLarge),
+              Text(tForgetWithPhoneNumberSubTitle,
+                  style: Theme.of(context).textTheme.titleSmall),
+              const SizedBox(height: tDefaultSize - 10),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.phone),
+                  hintText: "Phone Number",
+                  border: OutlineInputBorder(),
                 ),
-                const SizedBox(height: tDefaultSize - 10),
-                SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => OtpScreen(),
-                        ),
+              ),
+              const SizedBox(height: tDefaultSize - 10),
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => OtpScreen(),
                       ),
-                      child: Text(
-                        "Continue".toUpperCase(),
-                      ),
-                    )),
-              ],
-            ),
+                    ),
+                    child: Text(
+                      "Continue".toUpperCase(),
+                    ),
+                  )),
+            ],
           ),
         ),
       ),
--- lib/src/features/authentication/screens/forget password/forget_password_with_email.dart
+++ lib/src/features/authentication/screens/forget password/forget_password_with_email.dart
@@ -10,42 +10,41 @@ class ForgetPasswordWithEmail extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(tDefaultSize),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Icon(Icons.email, size: 120),
-                Text(tForgetWithEmailTitle,
-                    style: Theme.of(context).textTheme.titleLarge),
-                Text(tForgetWithEmailSubTitle,
-                    style: Theme.of(context).textTheme.titleSmall),
-                const SizedBox(height: tDefaultSize - 10),
-                TextFormField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    hintText: "Email address",
-                    border: OutlineInputBorder(),
+        body: Padding(
+          padding: const EdgeInsets.all(tDefaultSize),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(Icons.email, size: 120),
+              Text(tForgetWithEmailTitle,
+                  style: Theme.of(context).textTheme.titleLarge),
+              Text(tForgetWithEmailSubTitle,
+                  style: Theme.of(context).textTheme.titleSmall),
+              const SizedBox(height: tDefaultSize - 10),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  hintText: "Email address",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: tDefaultSize - 10),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => OtpScreen(),
+                    ),
+                  ),
+                  child: Text(
+                    "Continue".toUpperCase(),
                   ),
                 ),
-                const SizedBox(height: tDefaultSize - 10),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => OtpScreen(),
-                      ),
-                    ),
-                    child: Text(
-                      "Continue".toUpperCase(),
-                    ),
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
